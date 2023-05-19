2a30f54f8bc64ecebc32cc5f3bc1bdc6149deb89
skibanj <jubabanjo@gmail.com>
2023-05-19 02:03:53 +0000

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints
+ * Return: Always Success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
