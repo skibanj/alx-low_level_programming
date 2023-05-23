d9560686942e2192c4347aaa3c086fba14a32bb1
skibanj <jubabanjo@gmail.com>
2023-05-23 14:06:35 +0000

    numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count = 0;
+
+	while (count <= 9)
+	{
+		printf("%d", count);
+
+		count++;
+	}
+
+	printf("\n");
+
+
+	return (0);
+}
