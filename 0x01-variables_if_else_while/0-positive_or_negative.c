76c73d8bf8b1a432f0c7d67f002b41794410deba
skibanj <jubabanjo@gmail.com>
2023-05-23 01:54:17 +0000

    generate random numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,11 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * A program that prints random numbers
+ *
+ */
+
 int main(void)
 {
 	int n;
@@ -12,10 +16,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if ( n == 0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-	
+
 	return (0);
 }
