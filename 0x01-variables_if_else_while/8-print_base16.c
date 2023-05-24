9de2e9961178b1e431588618ecb0961cbae3d070
skibanj <jubabanjo@gmail.com>
2023-05-24 01:55:28 +0000

    base 16 in lower case

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,22 +3,26 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int i;
-	for (i = 0; i < 16; i++)
-	{
-		if (i < 10)
-			putchar('0' + i);
-	}
-	else 
-	{
-		putchar('a' + i - 10);
-	}
+	char c;
 
+	int d;
+
+	c = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
 	putchar('\n');
-
 	return (0);
 }
