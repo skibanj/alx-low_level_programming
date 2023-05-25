32086a20a73b6850bbea0a21f47b3ad0e66b475b
skibanj <jubabanjo@gmail.com>
2023-05-25 00:05:49 +0000

    alphabeth

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print alphabeth
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		S
+		a++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
