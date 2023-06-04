2879fa45a76a80a87695e8c6e006a425c12581d6
skibanj <jubabanjo@gmail.com>
2023-06-04 14:49:43 +0000

    sum of multiples of 3 or 5

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - a program that finds and prints the first 98 Fibonacci numbers 
+ * main - a program that finds and prints the first 98 Fibonacci numbers
  *
  * Return: Always 0 (Success)
  */
