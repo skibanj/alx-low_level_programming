ac48a3bbf04e9b3c6281af8326a9ec0cfc4877ef
skibanj <jubabanjo@gmail.com>
2023-06-09 08:19:09 +0000

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - A function that prints more numbers
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 9; i++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+		_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
