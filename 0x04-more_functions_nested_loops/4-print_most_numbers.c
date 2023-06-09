39c70926dbf0803373f18350f9950ab9bab5cd97
skibanj <jubabanjo@gmail.com>
2023-06-09 07:04:30 +0000

    omitted 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - a functions that print from 0 to 9 omitting 2 and 4
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (!(i == '2' || i == '4'))
+
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
