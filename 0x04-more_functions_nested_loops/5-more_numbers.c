b6580e4be2a4dda0d45dfba55e490727859a3382
skibanj <jubabanjo@gmail.com>
2023-06-09 09:30:35 +0000

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,19 @@
 void more_numbers(void)
 {
 	int i, j;
+	int num;
 
-	for (j = 0; j <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (i >= 10)
-			_putchar('1');
-			_putchar(i % 10 + '0');
+			num = j;
+			if (num >= 10)
+			{
+				_putchar('0' + (num / 10));
+				num %= 10;
+			}
+			_putchar('0' + num);
 		}
 		_putchar('\n');
 	}
