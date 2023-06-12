6d27864cc8e249c549855bf85b495efe6ed1589f
skibanj <jubabanjo@gmail.com>
2023-06-12 08:29:34 +0000

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - A function that prints a string
+ * @s: Mian character
+ * Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++
+	_puts_recursion(s);
+}
+
