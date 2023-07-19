5d05a4b1fcebbe58b3f11c290570b1d6767c9784
skibanj <jubabanjo@gmail.com>
2023-07-19 02:35:44 +0000

    file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - printing name of the file
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void){
 	printf("This program was compiled from the file: %s\n", __FILE__);
