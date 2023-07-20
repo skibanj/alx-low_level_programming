339f0f50d1b767ed5c32c71d9a3e2987c05bd4c1
skibanj <jubabanjo@gmail.com>
2023-07-20 15:05:31 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,22 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int i, j;
 
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
+	j = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		j++;
+		j++;
+	}
+
+	dest[i] = '\0';
 	return (dest);
 }
